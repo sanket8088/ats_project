1ad3a9e2e0fcfaa09e9ae4ca020535f776c8afe0
Sanket <32214164+sanket8088@users.noreply.github.com>
2024-06-27 01:23:05 +0530

    added sql query for word matching

--- ats/views/name_search_view.py
+++ ats/views/name_search_view.py
@@ -3,9 +3,11 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from ats.models import Candidate
 from ats.serializers import CandidateSerializer
-from django.db.models import Q, Value, CharField, IntegerField
+from django.db.models import Q, Value, CharField, IntegerField, Count
 from ats.constants import ErrorMessages
 from django.db.models.functions import Length
+from django.db import connection
+
 
 class NameSearchView(APIView):
     def get(self, request, *args, **kwargs)->Response:
@@ -13,26 +15,22 @@ class NameSearchView(APIView):
         if not query:
             return Response({"error": ErrorMessages.QUERY_PARAMS_NOT_FOUND.value}, status=status.HTTP_400_BAD_REQUEST)
 
-        query_tokens = query.lower().split()
+        get_query_data = self._get_query_data(query)
 
-        # Initialize a Q object for dynamic query construction
-        query_filter = Q()
-        for token in query_tokens:
-            query_filter |= Q(name__icontains=token)
-
-        # Find exact match candidates
-        exact_match_candidates = Candidate.objects.filter(name__iexact=query).annotate(
-            matching_character_count=Value(len(query), output_field=IntegerField())
-        ).order_by('-matching_character_count')
-
-        # Find partial match candidates, excluding exact matches, and annotate matching_character_count
-        partial_match_candidates = Candidate.objects.filter(query_filter).annotate(
-            matching_character_count=Length('name')
-        ).exclude(
-            id__in=exact_match_candidates.values_list('id', flat=True)
-        ).order_by("-matching_character_count")
-        combined_candidates = list(exact_match_candidates) + list(partial_match_candidates)
-        serializer = CandidateSerializer(combined_candidates, many=True)
-        serialized_candidates = serializer.data
-
-        return Response(serialized_candidates)
+        return Response(get_query_data)
+    
+    def _get_query_data(self, query :str)->list:
+        all_columns = ",".join([field.name for field in Candidate._meta.get_fields()])
+        with connection.cursor() as cursor:
+            sql_query = f"""SELECT {all_columns}, COUNT(*) AS match_count
+            FROM {Candidate._meta.db_table}
+            CROSS JOIN unnest(string_to_array('{query}', ' ')) AS word
+            WHERE name ILIKE '%' || word || '%'
+            GROUP BY {all_columns}
+            ORDER BY match_count DESC;"""
+            cursor.execute(sql_query)
+            columns = [col[0] for col in cursor.description]
+            rows = cursor.fetchall()
+            results = [dict(zip(columns, row)) for row in rows]
+        cursor.close()
+        return results
